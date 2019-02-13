d3e12ca1c409374b227b8e6f2fcf12189cabcea5
A. Sacman <44899859+sacman08@users.noreply.github.com>
2019-02-13 09:19:57 -0500

    Update FindSundaysTest.js

--- FindSundaysTest.js
+++ FindSundaysTest.js
@@ -3,6 +3,12 @@ var today = new Date();
 
 function crunchit()
 {
+  for (var year = 2014; year <= 2050; year++)
+      {
+      var d = new Date(year, 0, 1);
+      if ( d.getDay() === 0 )
+          console.log("1st January is being a Sunday  "+year);
+      }
 a1 = input.getFullYear();
 if (a1 < 2050)
   {
